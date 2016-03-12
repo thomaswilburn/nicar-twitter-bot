8f93a43dac742c922f8f198e2fd2118add94d1b9
Thomas Wilburn <news@thomaswilburn.net>
2016-03-12 13:26:44 -0800

    Update the proposed steps

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var emojify = require("./emojify");
 
-// 1. Get the current user's ID from users/lookup
 
-// 2. Get the user's followers with followers/ids
+// 1. Get the user's followers with followers/ids
 
-// 3. Connect to the stream for those followers using statuses/filter
+// 2. Connect to the stream for those followers using statuses/filter
 
-// 4. On data, reply to tweets matching a given phrase with statuses/update
+// 3. On data, reply to tweets matching a given phrase with statuses/update
